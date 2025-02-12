8f6d71c8bc10b289c852aaa75205405de2b72dc8
Tom Bum Su Choi <qjatn0545@naver.com>
2025-02-12 20:11:29 +0900

    메인 화면 하단 네비게이션 바를 BottomAppBar로 변경

--- lib/MainScreen.dart
+++ lib/MainScreen.dart
@@ -11,27 +11,25 @@ class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'This is an app',
-          style: TextStyle(color: Colors.white),
+        appBar: AppBar(
+          title: Text(
+            'This is an app',
+            style: TextStyle(color: Colors.white),
+          ),
+          backgroundColor: Colors.blue,
+          centerTitle: false,
         ),
-        backgroundColor: Colors.blue,
-        centerTitle: false,
-      ),
-      body: Text('안녕'),
-      bottomNavigationBar: Container(
-        padding: EdgeInsets.symmetric(vertical: 20),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Icon(Icons.phone),
-            Icon(Icons.message),
-            Icon(Icons.contact_page)
-          ],
-        ),
-      ),
-    );
+        body: Text('안녕'),
+        bottomNavigationBar: BottomAppBar(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Icon(Icons.phone),
+              Icon(Icons.message),
+              Icon(Icons.contact_page)
+            ],
+          ),
+        ));
   }
 }
 // 하단 네비게이션 바 위에 회색 그거 줄 해야함
