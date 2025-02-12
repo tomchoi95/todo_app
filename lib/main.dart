e3a6abba9c0c696c36d2d8b6f8242af01fd62e81
Tom Bum Su Choi <qjatn0545@naver.com>
2025-02-12 16:48:20 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(TodoApp());
+}
+
+class TodoApp extends StatelessWidget {
+  const TodoApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.light(useMaterial3: true),
+      home: const MainScreen(),
+    );
+  }
+}
