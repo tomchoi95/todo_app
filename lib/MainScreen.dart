0f3d763e9d4114b294fc5cbf2c6094a8c2f81a87
Tom Bum Su Choi <qjatn0545@naver.com>
2025-02-12 16:59:39 +0900

    메인 화면 추가 및 기본 설정 구성

--- lib/MainScreen.dart
+++ lib/MainScreen.dart
@@ -1,2 +1,23 @@
 import 'package:flutter/material.dart';
 
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Main Screen'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Text('Hello, World!'),
+      ),
+    );
+  }
+}
